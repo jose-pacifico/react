11ab98def9ebb1c6b2c61a1107e632df2bf40356
Jose Pacifico <alberto14.filho@gmail.com>
2023-07-13 18:41:25 -0400

    Adicionando o componente de Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import React, { Component } from 'react'
 import Table from './Table'
+import Navbar from './Navbar'
+import Cards from './Cards'
 
 class App extends Component {
     render() {
         return (
             <div className="container text-center">
-                <Table/>
-            </div>
+                <Navbar/>
+                <div class="row">
+                    <div class="col">
+                        <Cards/>
+                    </div>
+                    <div class="col">
+                        <Cards/>
+                    </div>
+                    <div class="col">
+                        <Cards/>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <Cards/>
+                    </div>
+                    <div class="col">
+                        <Cards/>
+                    </div>
+                    <div class="col">
+                        <Cards/>
+                    </div>
+                </div>
+              </div>
         )
     }
 }
---
+++ src/Cards.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Cards extends Component {
+    render() {
+        return (
+            <div className="card" >
+              <img src='https://m.media-amazon.com/images/I/71O-Il088wL._AC_UF894,1000_QL80_.jpg' 
+              className="card-img-top" 
+              />
+              <div classNameName="card-body">
+                <h5 className="card-title">Boneco Grogu - The Mandalorian</h5>
+                <p className="card-text">R$500,00</p>
+              </div>
+          </div>
+        )
+    }
+}
+
+export default Cards
